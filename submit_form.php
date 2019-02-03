0dcb9d70ba2166b64a6d9c8cedbf5ce9aac9122a
rahulvenneers <rahulvenneers@gmail.com>
2019-02-03 21:15:16 +0530

    update

--- submit_form.php
+++ submit_form.php
@@ -7,17 +7,30 @@ $no_guest=null;
 if(isset($_POST['no_guest']))
 	$no_guest=strip_tags(trim($_POST['no_guest']));
 
-$recipient = "deepu.divya.deepu@gmail.com";
-$subject = "Marriage RSVP";
-	$message="Hi Sanchit, \r\n\r\n";
-	$message.=$name." here, ";
-	($event_name=='not')?$message.=" I will not be attending your marriage due to some reasons. Wish you a very Happy Married life":$message.=" I will be attending ".$event_name;
-	($no_guest==null||$no_guest=="")?$message.=".":$message.=" with ".$no_guest." guests.\r\n \r\n";
-	$message.="\r\n\r\nRegards\r\n";
-	$message.=$name."\r\n";
-	$message.=$form_mobile;
-	$email_content .= "Message:\r\n$message\n";
+require 'PHPMailerAutoload.php';
+$mail = new PHPMailer;
 
+
+$mail->setFrom('deepu.divya.deepu@gmail.com');
+$mail->addAddress('deepu.divya.deepu@gmail.com', 'Deepak Patole');
+$mail->Subject  = 'Marriage RSVP';
+// $recipient = "deepu.divya.deepu@gmail.com";
+// $subject = "Marriage RSVP";
+// 	$message="Hi Sanchit, \r\n\r\n";
+// 	$message.=$name." here, ";
+// 	($event_name=='not')?$message.=" I will not be attending your marriage due to some reasons. Wish you a very Happy Married life":$message.=" I will be attending ".$event_name;
+// 	($no_guest==null||$no_guest=="")?$message.=".":$message.=" with ".$no_guest." guests.\r\n \r\n";
+// 	$message.="\r\n\r\nRegards\r\n";
+// 	$message.=$name."\r\n";
+// 	$message.=$form_mobile;
+// 	$email_content .= "Message:\r\n$message\n";
+$mail->Body ="hi";
+if(!$mail->send()) {
+  echo 'Message was not sent.';
+  echo 'Mailer error: ' . $mail->ErrorInfo;
+} else {
+  echo 'Message has been sent.';
+}
 	
 	
 
@@ -26,17 +39,17 @@ $subject = "Marriage RSVP";
         //$email_headers = "From: $name <$email>";
 
 
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            header('Content-Type: application/json');
-            echo json_encode(["message"=>"Thank You! Your message has been sent."]);
-        } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            header('Content-Type: application/json');
-            echo json_encode(["message"=>"Oops! Something went wrong and we couldn't send your message."]);
-        }
+//         if (mail($recipient, $subject, $email_content, $email_headers)) {
+//             // Set a 200 (okay) response code.
+//             http_response_code(200);
+//             header('Content-Type: application/json');
+//             echo json_encode(["message"=>"Thank You! Your message has been sent."]);
+//         } else {
+//             // Set a 500 (internal server error) response code.
+//             http_response_code(500);
+//             header('Content-Type: application/json');
+//             echo json_encode(["message"=>"Oops! Something went wrong and we couldn't send your message."]);
+//         }
 
 
 
